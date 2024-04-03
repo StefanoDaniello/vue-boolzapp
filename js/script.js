0647e814a663704e686cf2c06525b894e902a7f9
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-03 16:17:15 +0200

    add responsive message

--- js/script.js
+++ js/script.js
@@ -4,13 +4,24 @@ const {createApp} = Vue;
                                                                                                 
 createApp({
     data() {
+      
         return {
             contacts,
-            newMessage:''
+            newMessage:'',
+            prova:false,
+            messages:[]
+
         }
     },
     methods: {
-        
+        sendMessage() {
+
+            if (this.newMessage.trim() !== '') {
+              this.messages.push(this.newMessage);
+              this.prova = true
+              this.newMessage = '';
+            }
+        },
     },
     computed: {
     }
