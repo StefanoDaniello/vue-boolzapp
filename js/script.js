763c99dfd7564056c40b54bc55131782d4e145ab
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-03 23:04:34 +0200

    add responsive img

--- js/script.js
+++ js/script.js
@@ -24,15 +24,26 @@ createApp({
               this.messages.push(this.newMessage);
               this.bgmessage = true;
               this.newMessage = '';
+              this.data= new Date().toLocaleTimeString();
             }
             setTimeout(() => {
                 this.bgmessage2 = true;
                 this.saluto='ciao';
-            }, 1000);
+                this.data= new Date().toLocaleTimeString();
+            }, 4000);
+        },
+        sendMessage2(){
+
         },
         active(id) {
-            this.utenteactive= id;
-        },
+            this.utenteactive = id;
+        }
+        
+    },
+    mounted() {
+        if (this.utenteactive === null) {
+            this.utenteactive = this.contacts[0].id;
+        }
     },
     computed: {
         findUser() {
