68ccbdd9c7289210bb4f2d46286ee3ed614c4370
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-03 13:38:28 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import {contacts} from "./data.js";
+
+const {createApp} = Vue;
+                                                                                                
+createApp({
+    data() {
+        return {
+            contacts
+        }
+    },
+    methods: {
+    },
+    computed: {
+    }
+}).mount('#app')
